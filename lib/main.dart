3fef1f5ecdfc0d42e47b978bfcbaf7b590d421be
Abhay <xpectcorps@gmail.com>
2024-03-30 22:53:56 +0530

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Kerala Wings',
       debugShowCheckedModeBanner: false,
 
-      home: ProfileSetupScreen()
+      home: SplashScreen()
     );
   }
 }
